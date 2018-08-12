152f0d598dc6841c77859fb6bafe6c9531505a3c
Adam Oswalt <email@adamoswalt.com>
2018-08-12 01:19:17 -0500

    Rough version of simple bounce

--- src/canvas.js
+++ src/canvas.js
@@ -1,14 +1,20 @@
 const canvas = document.querySelector('canvas')
 
-export const resizeCanvas = (width, height) => {
+export const ctx = canvas.getContext('2d')
+
+export const resize = (width, height) => {
   canvas.width = width
   canvas.height = height
 }
 
-const fullscreenCanvas = () =>
-  resizeCanvas(window.innerWidth, window.innerHeight)
+export const clear = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+const makeFullscreen = () =>
+  resize(window.innerWidth, window.innerHeight)
 
 export const autoResizeCanvas = () => {
-  fullscreenCanvas()
-  window.addEventListener('resize', fullscreenCanvas)
+  makeFullscreen()
+  window.addEventListener('resize', makeFullscreen)
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-import { autoResizeCanvas } from './canvas'
+import { autoResizeCanvas, clear, ctx } from './canvas'
 
 autoResizeCanvas()
+
+const drag = 0.8
+const gravity = 1
+
+const atGround = yPos => yPos >= innerHeight
+
+const update = shape => {
+  const { x, y, speedX, speedY, radius } = shape
+
+  const nextX = x + speedX
+  const nextY = y + speedY + gravity
+
+
+  const newSpeedX = speedX
+  // NOTE(adam): bounce off of ground with drag
+  const newSpeedY =  atGround(nextY + radius)
+    ? -speedY * drag
+    : speedY + gravity
+
+  return {
+    ...shape,
+    x: x + newSpeedX,
+    y: y + newSpeedY,
+    speedX: newSpeedX,
+    speedY: newSpeedY,
+  }
+}
+
+const render = ({ x, y, radius, color }) => {
+  ctx.beginPath()
+  ctx.arc(x, y, radius, 0, Math.PI * 2)
+  ctx.fillStyle = color
+  ctx.fill()
+  ctx.closePath
+}
+
+const animate = (shapes) =>
+  shapes
+    .map(update)
+    .map(s => render(s) || s)
+
+const renderLoop = (shapes) => {
+  clear()
+  const updatedShapes = animate(shapes)
+  requestAnimationFrame(() => renderLoop(updatedShapes))
+}
+
+const initShapes = [
+  {
+    x: 200,
+    y: 200,
+    speedX: 0,
+    speedY: 0,
+    color: 'blue',
+    radius: 50,
+  },
+]
+
+renderLoop(initShapes)

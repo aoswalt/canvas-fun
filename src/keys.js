1f063aad9176e1d3466d5e9b6e470452059a12a7
Adam Oswalt <email@adamoswalt.com>
2019-01-07 21:34:59 -0600

    Add key definitions

--- src/keys.js
+++ src/keys.js
@@ -15,3 +15,17 @@ export const getKeysDown = () => [...keysDown]
 export const isKeyDown = key => keysDown.includes(key)
 export const areKeysDown = (...keys) => keys.every(isKeyDown)
 export const anyKeysDown = (...keys) => keys.some(isKeyDown)
+
+export const UP = 'ArrowUp'
+export const DOWN = 'ArrowDown'
+export const LEFT = 'ArrowLeft'
+export const RIGHT = 'ArrowRight'
+export const SPACE = ' '
+export const ENTRER = 'Enter'
+export const SHIFT = 'Shift'
+export const CTRL = 'Control'
+export const ALT = 'Alt'
+export const CAPS = 'CapsLock'
+export const TAB = 'Tab'
+export const BACKSPACE = 'Backspace'
+export const DELETE = 'Delete'

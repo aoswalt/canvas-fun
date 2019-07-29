49bdfdca9a5d3be8d2fcb992d487c99b81323b07
Adam Oswalt <email@adamoswalt.com>
2019-07-28 19:37:48 -0500

    Deallocate entities after lifespan

--- src/systems/aging.js
+++ src/systems/aging.js
@@ -1,6 +1,33 @@
 import Vector from '../Vector'
+import { deallocate, get, isAlive } from '../generationalIndexing'
 import { setupSystem } from '../systems'
+import pipe from '../pipe'
 
 const growOlder = ({ age }) => ({ age: { ...age, current: age.current + 1 } })
 
-export default setupSystem(['age'], growOlder)
+const aging = setupSystem(['age'], growOlder)
+
+const expiration = world => {
+  const expired = world.entities
+    .filter(gi => isAlive(world.allocator, gi))
+    .filter(gi => {
+      const age = get(world.age, gi)
+      return age && age.current > age.lifespan
+    })
+
+  if(!expired.length) return world
+
+  return expired.reduce(
+    (currentWorld, gi) => ({
+      ...currentWorld,
+      allocator: deallocate(currentWorld.allocator, gi),
+    }),
+    world,
+  )
+}
+
+export default world =>
+  pipe(world)
+    .p(aging)
+    .p(expiration)
+    .value()
